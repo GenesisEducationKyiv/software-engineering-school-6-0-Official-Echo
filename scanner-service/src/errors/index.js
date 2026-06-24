e0afc1d0a4c6549d60bf901cad692b9d90b20da9
Official-Echo <o.zolotarov@ukma.edu.ua>
2026-06-25 02:18:12 +0300

    refactor: Move error utilities to common package

--- scanner-service/src/errors/index.js
+++ scanner-service/src/errors/index.js
@@ -1,14 +1,1 @@
-export class AppError extends Error {
-	constructor(message, code) {
-		super(message);
-		this.name = this.constructor.name;
-		this.code = code;
-	}
-}
-
-export class RateLimitError extends AppError {
-	constructor(message, code, retryAfter = 60) {
-		super(message, code);
-		this.retryAfter = retryAfter;
-	}
-}
+export * from "@ghchk/common/errors/index.js";

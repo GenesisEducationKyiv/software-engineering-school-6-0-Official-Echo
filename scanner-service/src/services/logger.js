e66435e944964b650d00ded98031d97949dc2c50
Official-Echo <o.zolotarov@ukma.edu.ua>
2026-07-13 02:35:21 +0300

    refactor: Add extension

--- scanner-service/src/services/logger.js
+++ scanner-service/src/services/logger.js
@@ -1,3 +1,3 @@
-import { logger as defLogger } from "@ghchk/common/services/logger";
+import { logger as defLogger } from "@ghchk/common/services/logger.js";
 
 export const logger = defLogger.child({ server: "scanner" });
